01e3428282a41514b7f67bedfa4437db70710f36
Ivan Angelov <ivanangelov414@gmail.com>
2024-07-23 21:57:52 +0300

    Yet another modification

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    host: '0.0.0.0',
     port: process.env.PORT || 4173 
   }
 })
