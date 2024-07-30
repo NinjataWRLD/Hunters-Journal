8be80ac25c93b7f7f62c6a136188cd8de2a2a12d
Ivan Zlatinov <vani.zlatinov51@gmail.com>
2024-07-30 14:27:46 +0300

    Added components

---
+++ frontend/src/layout/Footer.jsx
@@ -0,0 +1,21 @@
+import '/css/styles.css';
+
+function Footer() {
+    return (
+        <>
+        <footer>
+            <p>&copy; 2024 Demo. All rights reserved.</p> |
+            <p>
+              Follow us on
+              <a href="https://twitter.com"> Twitter </a> and
+              <a href="https://instagram.com"> Instagram</a>
+            </p> |
+            <p>
+              <a href="/privacy-policy">Privacy Policy</a>
+            </p>
+          </footer>
+        </>
+    );
+}
+
+export default Footer;
