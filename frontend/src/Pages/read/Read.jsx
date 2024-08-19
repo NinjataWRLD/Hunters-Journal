b5892f9e64565f2d4e5cb620f198a3073b0d917d
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2024-08-19 17:20:08 +0300

    implemented read.jsx

--- frontend/src/Pages/read/Read.jsx
+++ frontend/src/Pages/read/Read.jsx
@@ -1,12 +1,25 @@
-import { useContext } from "react";
-import Context from "../../context";
+import { useState, useEffect } from "react";
+import ReadItem from "./ReadItem"
 
 function Read() {
-    return  (
-        <>
+    const [hellhounds, setHellhounds] = useState([]);
+    useEffect(() => {
+        fetchHellhounds();
+    }, []);
 
+    return (
+        <>
+            <ul>
+                {hellhounds.map(hellhound => <li key={hellhound._id}><ReadItem hellhound={hellhound} /></li>)}
+            </ul>
         </>
     );
+
+    async function fetchHellhounds() {
+        await fetch(import.meta.env.VITE_API_URL + "/api/hellhounds")
+            .then(response => response.json())
+            .then(data => setHellhounds(data));
+    }
 }
 
 export default Read;
