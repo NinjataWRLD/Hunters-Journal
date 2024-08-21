a04214346c826bcc19667be6b07a44854aab7b2f
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2024-08-21 10:53:10 +0300

    finished browse page

--- frontend/src/Pages/read/Read.jsx
+++ frontend/src/Pages/read/Read.jsx
@@ -1,17 +1,45 @@
 import { useState, useEffect } from "react";
 import ReadItem from "./ReadItem"
+import { useContext } from 'react';
+import Context from "../../context";
+import './Read.css'
 
 function Read() {
     const [hellhounds, setHellhounds] = useState([]);
+    const { isPlaying, setVolume } = useContext(Context);
+
     useEffect(() => {
         fetchHellhounds();
     }, []);
 
     return (
         <>
-            <ul>
-                {hellhounds.map(hellhound => <li key={hellhound._id}><ReadItem hellhound={hellhound} /></li>)}
-            </ul>
+            <div className="content">
+                <h2>Available creatures:</h2>
+                <div className="items">
+                    {hellhounds.map(hellhound => (
+                        <div id="hell_list" key={hellhound._id}>
+                            <ReadItem hellhound={hellhound} />
+                        </div>
+                    ))}
+                </div>
+                <h3>Click to edit</h3>
+            </div>
+
+            {isPlaying && (
+                <div className="volume-control">
+                    <label htmlFor="volume">Volume:</label>
+                    <input
+                        id="volume"
+                        type="range"
+                        min="0"
+                        max="1"
+                        step="0.01"
+                        defaultValue="0.8"
+                        onChange={setVolume}
+                    />
+                </div>
+            )}
         </>
     );
 
