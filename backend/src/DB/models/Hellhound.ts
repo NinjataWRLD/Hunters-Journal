ab09dfea574339cf2734e084e8cec9aa17e0e9cd
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2024-09-02 22:20:41 +0300

    forgot to change to strings

--- backend/src/DB/models/Hellhound.ts
+++ backend/src/DB/models/Hellhound.ts
@@ -7,19 +7,19 @@ interface IHellhound {
   hp: number;
   age: number;
   rarity: string;
-  strengths: string[];
-  weakness: string[];
+  strength: string;
+  weakness: string;
 }
 
 const hellhoundSchema = new Schema<IHellhound>({
   image: { type: String },
   name: { type: String, required: true },
-  invisibility: { type: Boolean, required: true },
+  invisibility: { type: Boolean, required: true }, //true, false
   hp: { type: Number, required: true }, // 0-100
-  age: { type: Number, required: true },
+  age: { type: Number, required: true }, //0-10000
   rarity: { type: String, required: true }, // Common, Rare, Epic, Legendary
-  strengths: { type: [String], required: true },  // 1-50
-  weakness: { type: [String], required: true },
+  strength: { type: String, required: true },
+  weakness: { type: String, required: true },
 });
 
 const Hellhound = model<IHellhound>('Hellhound', hellhoundSchema);
