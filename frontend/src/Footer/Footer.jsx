85f51dc1e83bd901f9c26153221965ae424c9521
ivanzlatinov1 <ivanzlatinov006@gmail.com>
2024-09-02 19:28:51 +0300

    modified create page

--- frontend/src/Footer/Footer.jsx
+++ frontend/src/Footer/Footer.jsx
@@ -7,8 +7,8 @@ function Footer() {
             <p>&copy; 2024 Demo. All rights reserved.</p> |
             <p>
               Follow us on
-              <a target="_blank" href="https://twitter.com"> Twitter </a> and
-              <a target="_blank" href="https://instagram.com"> Instagram</a>
+              <a target="_blank" href="https://x.com/customcads/"> Twitter </a> and
+              <a target="_blank" href="https://www.instagram.com/custom_cads/"> Instagram</a>
             </p> |
             <p>
               <a target="_blank" href="/privacy-policy">Privacy Policy</a>
--- frontend/src/Pages/monsters/create/Create.jsx
+++ frontend/src/Pages/monsters/create/Create.jsx
@@ -1,9 +1,87 @@
 import axios from "axios";
+import { useForm } from "react-hook-form"
+import "./Create.css"
 
 function Create() {
-    return  (
-        <>
+    const { register, handleSubmit, formState } = useForm();
 
+    async function onSubmit(data) {
+        const dto = { ...data };
+        await axios.post(`https://mern-demo-backend-lfjj.onrender.com/api/hellhounds`, dto);
+    }
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="firstCol">
+                <label htmlFor="name">Name:</label>
+                <input
+                    id="name"
+                    type="text"
+                    {...register("name")}
+                    autoComplete="off"
+                />
+                <label htmlFor="age">Age:</label>
+                <input
+                    id="age"
+                    type="number"
+                    {...register("age")}
+                    autoComplete="off"
+                />
+                <label htmlFor="hp">Health:</label>
+                <input
+                    id="hp"
+                    type="number"
+                    {...register("hp")}
+                    autoComplete="off"
+                />
+                <label htmlFor="rarity">Rarity:</label>
+                <input
+                    id="rarity"
+                    type="text"
+                    {...register("rarity")}
+                    autoComplete="off"
+                />
+                </div>
+                <div className="secondCol">
+                <label htmlFor="invisibility">Invisibility:</label>
+                <input
+                    id="invisibility"
+                    type="text"
+                    {...register("invisibility")}
+                    autoComplete="off"
+                />
+                <label htmlFor="image">Image URL:</label>
+                <input
+                    id="image"
+                    type="text"
+                    {...register("image_url")}
+                    autoComplete="off"
+                />
+                </div>
+                <div className="thirdCol">
+                <label htmlFor="strength">Power and Abilities:</label>
+                <input
+                    id="strength"
+                    type="textarea"
+                    {...register("strength")}
+                    autoComplete="off"
+                    className="large"
+                />
+                <label htmlFor="weakness">Weaknesses:</label>
+                <input
+                    id="weakness"
+                    type="textarea"
+                    {...register("weakness")}
+                    autoComplete="off"
+                    className="large"
+                />
+                </div>
+                <input
+                    type="submit"
+                    value={"Submit"}
+                />
+            </form>
         </>
     );
 }
