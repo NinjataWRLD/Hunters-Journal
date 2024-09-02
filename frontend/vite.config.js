a415380fd58a33138fbfe42407a48d956094c30b
Ivan Angelov <ivanangelov414@gmail.com>
2024-09-03 00:08:26 +0300

    Removed linting and added '@' option for imports

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,17 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173 
+    port: process.env.PORT || 4173
   }
 })
